2cc2263476ff771f8188aa0ed571eee2e17d3158
alicia <220036@fppiramide.com>
2024-03-03 21:20:18 +0100

    meweejoras

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors"
 import { routerUsuarios } from "./usuarios/infrastructure/rest/usuarioRouter";
 import { routerLibros } from "./libros/infrastructure/rest/librosRouter";
 
@@ -7,8 +8,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const allowedOrigins = ["http://localhost:5173"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 app.use(express.json());
+app.use(cors(options));
 
 app.use("/api/usuarios", routerUsuarios)
 app.use("/api/libros", routerLibros)
--- src/libros/infrastructure/rest/librosRouter.ts
+++ src/libros/infrastructure/rest/librosRouter.ts
@@ -94,6 +94,7 @@ router.put("/:ejemplar", isAuth, async(req,res)=>{
         const emailUsuario = req.body.userEmail;
         const idLibro = parseInt(req.params.ejemplar)
         const prestamo = await librosUseCases.devolverLibro(idLibro,emailUsuario)
+        console.log(prestamo)
         res.json(prestamo)
 
     }catch(error){
